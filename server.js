2f04b4c2c7446b99f5820ccff1e30a6a975896ba
MrCaptainOG <gauravbhaiya740@gmail.com>
2025-09-15 17:33:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 5000;
 const SECRET = process.env.SECRET_CODE || "changeme";
 
 app.use(bodyParser.json());
